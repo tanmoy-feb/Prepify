8481e428ae3ec3996c33d40f2a5e2f8c014134e1
Himadri Das <dashimadri6789@gmail.com>
2025-07-05 21:17:22 +0530

    landing page done

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Landing() {
+  return (
+    <div style={{ background: "black", color: "white", padding: "50px" }}>
+      <h1>Hello from Landing Page</h1>
+    </div>
+  );
+}
